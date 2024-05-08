644495b954467a9bfed8d4984e1f6fd7f38567ea
Hemnath <hemd@paypal.com>
2024-05-08 23:23:56 +0530

    Return PONG for multiple clients

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,25 +4,37 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 
 public class Main {
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-        //  Uncomment this block to pass the first stage
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
         int port = 6379;
         try {
-            serverSocket = new ServerSocket(port);
+            ServerSocket serverSocket = new ServerSocket(port);
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
-            clientSocket = serverSocket.accept();
 
+            for(int i = 0; i < 10; i++) {
+                executorService.submit(() -> handleCommand(serverSocket));
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+
+    private static void handleCommand(ServerSocket serverSocket) {
+        Socket clientSocket = null;
+        try {
+            clientSocket = serverSocket.accept();
             PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream());
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String inputLine = in.readLine();
@@ -40,11 +52,9 @@ public class Main {
             System.out.println("IOException: " + e.getMessage());
         } finally {
             try {
-                if (clientSocket != null) {
-                    clientSocket.close();
-                }
-            } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
+                clientSocket.close();
+            } catch (IOException ioException) {
+                System.out.println(ioException.getMessage());
             }
         }
     }
