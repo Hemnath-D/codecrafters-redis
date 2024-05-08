25c08ef0db5cc83980d25d703ab6951e4f768199
Hemnath <hemd@paypal.com>
2024-05-08 19:26:38 +0530

    Reformat Code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,40 +5,41 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
 
-    //  Uncomment this block to pass the first stage
+public class Main {
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
+
+        //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         int port = 6379;
         try {
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
+            serverSocket = new ServerSocket(port);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
 
-          PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream());
-          BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-          String inputLine = in.readLine();
-          System.out.println(inputLine);
+            PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine = in.readLine();
+            System.out.println(inputLine);
 
-          printWriter.print("+PONG\r\n");
-          printWriter.flush();
+            printWriter.print("+PONG\r\n");
+            printWriter.flush();
         } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
-            }
-          } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-          }
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
         }
-  }
+    }
 }
