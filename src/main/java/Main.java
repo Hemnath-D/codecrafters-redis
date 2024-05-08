b739c1e4fa313f516da663f094bad400ea4ee750
Hemnath <hemd@paypal.com>
2024-05-08 19:20:34 +0530

    Respond PONG

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,6 +21,14 @@ public class Main {
           serverSocket.setReuseAddress(true);
           // Wait for connection from client.
           clientSocket = serverSocket.accept();
+
+          PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream());
+          BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+          String inputLine = in.readLine();
+          System.out.println(inputLine);
+
+          printWriter.print("+PONG\r\n");
+          printWriter.flush();
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
